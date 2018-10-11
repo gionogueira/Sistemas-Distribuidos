ca0e2e59f0be0dc745ac53e77d0d8bad867660b1
gionogueira <giovannarnogueira@gmail.com>
2018-10-11 01:15:01 -0300

    Criação do soap 'Calculadora' utilizando a biblioteca SOAPpy

---
+++ CalcSOAP/CalcSOAP.py
@@ -0,0 +1,14 @@
+from SOAPpy import SOAPServer
+
+def calcula(op1,op2,operacao):
+        if operacao == '+':
+                return op1 + op2
+        if operacao == '-':
+                return op1 - op2
+        if operacao == '*':
+                return op1 * op2
+        if operacao == '/':
+                return op1 / op2
+server = SOAPServer(('localhost',8081))
+server.registerFunction(calcula)
+server.serve_forever() # processa uma ou várias solicitações.
