904466eb3e11bdc91142626762c96b6fc82b332d
gionogueira <giovannarnogueira@gmail.com>
2018-12-02 17:42:01 -0300

    Criar MOM em python

---
+++ mom-py/send.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',routing_key='hello', body='Hello World!')
+channel.basic_publish(exchange='',routing_key='hello', body='Hello Again!')
+
+channel.queue_declare(queue='eai')
+channel.basic_publish(exchange='',routing_key='eai', body='Vai encarar?!')
+
+print("Sent Messages...")
+connection.close()
